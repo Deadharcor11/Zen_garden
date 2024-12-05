bdcd4e21cac66d40231c1b071b716b7566ffbc3c
Deadharcor11 <masterjedi11@outlook.com>
2024-12-04 21:45:18 -0600

    Pantalla de inicio y pantalla siguiente con boton funcional

--- app/src/main/java/com/upiicsapm/zen_garden/paginacons.kt
+++ app/src/main/java/com/upiicsapm/zen_garden/paginacons.kt
@@ -1,32 +1,20 @@
 package com.upiicsapm.zen_garden
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Pagina2 : AppCompatActivity() {
+class paginacons : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_pagina2)
+        setContentView(R.layout.activity_paginacons)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val btn2: Button = findViewById(R.id.button3)
-        btn2.setOnClickListener{
-            val intent: Intent = Intent(this, paginarecord::class.java)
-            startActivity(intent)
-        }
-        val btn3: Button = findViewById(R.id.button2)
-        btn3.setOnClickListener{
-            val intent: Intent = Intent(this, paginacons::class.java)
-            startActivity(intent)
         }
     }
 }
